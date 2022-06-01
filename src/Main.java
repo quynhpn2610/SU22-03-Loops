298bb255c992d33e51f3edad6a4b307b6b68494f
quynhpn2610 <quynhphuongnguyen.2610@gmail.com>
2022-06-01 20:14:47 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        boolean flag = true;
+        Scanner sc = new Scanner(System.in);
+        int prev = -1;
+        while(true){
+            int cur = sc.nextInt();
+            if (cur == 0){
+                break;
+            }
+            if (cur < prev){
+                flag = false;
+            }
+            prev = cur;
+        }
+        if(!flag){
+            System.out.print("NO");
+        }
+        else{
+            System.out.print("YES");
+        }
+    }
+}
